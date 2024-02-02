acd19bad2197b496ea0cd851eb50be9f3e9a3729
IlOvecompUter1$t <shahva2022@gmail.com>
2024-02-02 20:39:45 +0500

    datatype

---
+++ datatype.js
@@ -0,0 +1,32 @@
+/*
+there are seven primitive datatypes in javascript other are non-primitive(object)
+1.number/numaric
+2.string 
+3.boolean
+4.undefined
+5.null
+6.bigInt
+7.symbol
+*/
+
+
+// number/numaric
+let age = 12;
+
+// string 
+let fullName = "shahsawar Ali";
+
+// boolean
+let isFollow = true;
+
+// undefined by defualt all empty variable are undefined
+let x;
+
+// null
+let y = null;
+
+// bigInt
+let z = BigInt("1234")
+
+// symbol
+let a = symbol("Hello!")
